b628531689dd52fe22fa423eb568aa05bca7bbee
Pedro Renzo <pedro.renzo1@gmail.com>
2018-03-20 22:30:24 -0300

    Cliente, TipoCliente, Telefone e Endereco

--- src/main/java/com/pedro/cursomc/CursomcApplication.java
+++ src/main/java/com/pedro/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.pedro.cursomc.domain.Categoria;
 import com.pedro.cursomc.domain.Cidade;
+import com.pedro.cursomc.domain.Cliente;
+import com.pedro.cursomc.domain.Endereco;
 import com.pedro.cursomc.domain.Estado;
 import com.pedro.cursomc.domain.Produto;
+import com.pedro.cursomc.domain.enums.TipoCliente;
 import com.pedro.cursomc.repositories.CategoriaRepository;
 import com.pedro.cursomc.repositories.CidadeRepository;
+import com.pedro.cursomc.repositories.ClienteRepository;
+import com.pedro.cursomc.repositories.EnderecoRepository;
 import com.pedro.cursomc.repositories.EstadoRepository;
 import com.pedro.cursomc.repositories.ProdutoRepository;
 
@@ -21,6 +26,12 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
@@ -67,6 +78,18 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		estadoRepository.save(Arrays.asList(est1, est2));
 		cidadeRepository.save(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.save(cli1);
+		enderecoRepository.save(Arrays.asList(e1, e2));
+		
 	}
 	
 }
